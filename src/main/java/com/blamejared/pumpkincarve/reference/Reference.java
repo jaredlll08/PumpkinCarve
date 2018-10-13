e750ef0643e3a32ca428fe75e4977d0084f8a90f
Jared <jaredlll08@gmail.com>
2018-10-14 00:33:17 +0200

    reset

--- src/main/java/com/blamejared/pumpkincarve/reference/Reference.java
+++
@@ -1,9 +0,0 @@
-package com.blamejared.pumpkincarve.reference;
-
-public class Reference {
-    
-    public static final String MODID = "pumpkincarve";
-    public static final String NAME = "PumpkinCarve";
-    public static final String VERSION = "1.0.0";
-    
-}
